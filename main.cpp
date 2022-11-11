03603db96c10b29045f9b7750136de5536970644
Mastercineggro <116543040+Mastercineggro@users.noreply.github.com>
2022-11-11 22:26:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,6 @@ cin >> a >> b >> c;//input
 cout<<0.5*a*a<<endl;//area triangolo
 cout<<a*a<<endl;//area quadrato
 cout<<a*b<<endl;//area rettangolo
-cout<<0.5*(a+b)c<<endl;//area trapezio
+cout<<0.5*(a+b)*c<<endl;//area trapezio
   return 0;
 }
