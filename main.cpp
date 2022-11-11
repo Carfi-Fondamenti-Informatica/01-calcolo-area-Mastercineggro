e608b43b48fcb6b2ddf98c2ff1e6f472a4e97e34
Mastercineggro <116543040+Mastercineggro@users.noreply.github.com>
2022-11-11 22:22:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
-cin >> a >> b >> c;
+float a, b, c;//variabili
+cin >> a >> b >> c;//input
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+cout<<0.5*a*a<<endl;//area triangolo
+cout<<a*a<<endl;//area quadrato
+cout<<a*b<<endl;//area rettangolo
+cout<<0.5*(a+b)c<<endl;//area trapezio
+  return 0;
 }
