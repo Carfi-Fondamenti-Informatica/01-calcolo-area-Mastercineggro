f497f5b3749c47000dee2c05556bb71ad96fcdbf
Mastercineggro <116543040+Mastercineggro@users.noreply.github.com>
2022-11-11 22:28:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ float a, b, c;//variabili
 cin >> a >> b >> c;//input
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-cout<<0.5*a*a<<endl;//area triangolo
+cout<<0.5*a*b<<endl;//area triangolo
 cout<<a*a<<endl;//area quadrato
 cout<<a*b<<endl;//area rettangolo
 cout<<0.5*(a+b)*c<<endl;//area trapezio
